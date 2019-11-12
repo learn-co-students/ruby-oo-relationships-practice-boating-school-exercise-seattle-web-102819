11949ff07a1964def5dc0ac2d04a4c5e669e9360
bahayg <bahayg@gmail.com>
2019-11-11 17:26:47 -0800

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,26 @@
 class Instructor
+    attr_accessor :name
+    @@all = []
 
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def pass_student(student, test_name)
+       abc = BoatingTest.all.select do |boatingtest| 
+            boatingtest.student == student.first_name && boatingtest.test_name == test_name && boatingtest.instructor == self
+            abc.map {|boatingtest| boatingtest.status = "passed"}
+   
+                
+        #     else
+        #         BoatingTest.new(student, test_name, status="passed", self )
+        #     end
+        # BoatingTest
+        end
+    en
 end
--- tools/console.rb
+++ tools/console.rb
@@ -8,6 +8,17 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+spongebob = Student.new("Spongebob")
+patrick= Student.new("Patrick")
+
+puff= Instructor.new("Ms.Puff")
+krabs= Instructor.new("Mr.Krabs")
+
+no_crashing = BoatingTest.new(spongebob, "Don't Crash 101", "pending", puff)
+power_steering_failure = BoatingTest.new(patrick, "Power Steering 202", "failed", puff)
+power_steering_pass = BoatingTest.new(patrick, "Power Steering 201", "passed", krabs)
+no_crash = BoatingTest.new(spongebob, "Don't Crash", "passed", puff)
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
 
